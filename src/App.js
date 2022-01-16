b66ae8cbe63e774a59703cfe150fbdd81f825a33
Martin <esim_player@hotmail.com>
2022-01-16 13:41:51 +0100

    new change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 
 import './App.css';
-
+import Header from './components/Header';
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold  underline">
-      Hello 
-    </h1>
-    </div>
+    <Header/>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+             <h1 className="text-3xl font-bold  underline">
+                Hello 
+            </h1>
+        </div>
+    )
+}
+
+export default Header
