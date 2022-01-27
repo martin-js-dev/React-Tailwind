cc05a721d763e7ddbc8dcf9dd2978ae6545ad017
Martin <esim_player@hotmail.com>
2022-01-27 18:53:07 +0100

    new change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 
 import './App.css';
 import Header from './components/Header';
+import About from './components/About';
+import Feature from './components/Feature';
+import Product from './components/Product';
+import Offers from './components/Offers';
+import Testimonials from './components/Testimonials';
+import Blog from './components/Blog';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
 function App() {
   return (
-    <Header/>
+    <div>
+      <Header/>
+
+      <About/>
+      <Feature/>
+      <Product/>
+      <Offers/>
+      <Testimonials/>
+      <Blog/>
+      <Contact/>
+      <Footer/>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
+    colors: {
+      'primary': '#d2601a',
+      'secondary': '#1d3c45',
+      'light': '#fff1e1',
+      
+    },
+    fontFamily: {
+      'poppins': ['Poppins', ],
+      
+    },
     extend: {},
   },
   plugins: [],
